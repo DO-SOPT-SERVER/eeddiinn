4eaf9a465d2246f13e64ebc35e2045c0419665b1
eeddiinn <gdw4013@naver.com>
2023-10-13 00:41:19 +0900

    1주차 세미나 과제 제출

---
+++ semina/src/main/java/semina/semina/controller/HealthCheckController.java
@@ -0,0 +1,47 @@
+package semina.semina.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import semina.semina.dto.HealthCheckResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping("/v1")
+    public Map<String, String> healthCHeck(){
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return response;
+    }
+
+    @GetMapping("/v2")
+    public ResponseEntity<String> healthCheckV2()
+    {
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/v3")
+    public String healthCheckV3() {
+        return "OK";
+    }
+
+    @GetMapping("/v4")
+    public ResponseEntity<Map<String, String>> healthCheckV4(){
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "OK");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/v5")
+    public ResponseEntity<HealthCheckResponse> healthCheckv5()
+    {
+        return ResponseEntity.ok(new HealthCheckResponse("ok"));
+    }
+}
+
